2c4aacc3155667038907f9021a5833e3c05fc4ba
Bardo91 <19Bardo91@gmail.com>
2014-03-15 23:21:21 +0100

    MADE: --> Added more functionality
    TODO:
    --> GivenRotations
    --> QR
    --> EKF

--- source/TestMatrix.cpp
+++ source/TestMatrix.cpp
@@ -12,22 +12,42 @@ void testMatrix(){
 	std::cout << "TESTING MATRIX OPERATIONS" << std::endl;
 	double matArray[9] = {	1.0, 2.0, 3.0, 
 							4.0, 5.0, 6.0, 
-							7.0, 8.0, 9.0	};
-
+							7.0, 8.0, 8.0	};
+	std::cout << "Matrix creation" << std::endl;
 	BOViL::math::Matrix<double> mat1(matArray, 3, 3);
 	mat1.showMatrix();
 
+	std::cout << "Matrix asignation" << std::endl;
 	BOViL::math::Matrix<double> mat2;
 	mat2 = mat1;
 	mat2.showMatrix();
 
+	std::cout << "Matrix addition" << std::endl;
 	BOViL::math::Matrix<double> mat3 = mat1 + mat2;
 	mat3.showMatrix();
 
+	std::cout << "Matrix substraction" << std::endl;
 	BOViL::math::Matrix<double> mat4 = mat1 * mat2;
 	mat4.showMatrix();
 
+	std::cout << "Matrix transpose" << std::endl;
 	BOViL::math::Matrix<double> mat5 = !mat4;
 	mat5.showMatrix();
+
+	std::cout << "Matrix LU decomposition" << std::endl;
+	BOViL::math::Matrix<double> matL(3, 3), matU(3, 3);
+	if(mat1.decompositionLU(matL, matU)){
+		matL.showMatrix();
+		matU.showMatrix();
+	}
+
+	std::cout << "Matrix LU decomposition" << std::endl;
+	std::cout << "Determinant: " << ~mat1 << std::endl;
+
+	std::cout << "Norm" << std::endl;
+	BOViL::math::Matrix<double> mat6(matArray, 3, 1);	
+	mat6.showMatrix();
+	std::cout << "Norm: " << mat6.norm() << std::endl;
+
 
 }
--- source/core/math/Matrix.h
+++ source/core/math/Matrix.h
@@ -19,7 +19,7 @@ namespace BOViL{
 		//-----------------------------------------------------------------------------
 		template <typename type_> 
 		class Matrix{
-		public:
+		public:		// Main interface
 			Matrix();		// Default constructor
 			Matrix(int _cols, int _rows);		// Empty matrix constructor
 			Matrix(const type_* _mat, int _rows, int _cols);	// Full-defined matrix constructor
@@ -34,22 +34,32 @@ namespace BOViL{
 
 		public:	// Overloaded Operators
 			std::string operator<<(const Matrix<type_>& _mat) const;		// Operator for cout 666 TODO:
+			type_* operator[](int _index);
 			void operator=(const Matrix& _mat);				// Assignement operator
 			Matrix operator+(const Matrix& _mat) const;		// Add operator
 			Matrix operator-(const Matrix& _mat) const;		// Sub operator
 			Matrix operator*(const Matrix& _mat) const;		// Mul operator
+			Matrix operator&(const Matrix& _mat) const;		// Projection operator._mat is projected to this
 			Matrix operator!();								// Transpose operator
-			type_ operator~();								// Determinant operator		666 TODO:
+			type_ operator~();								// Determinant operator
 			Matrix operator^(const Matrix& _mat) const;		// Pow operator     666 TODO:
 
+		public:		// Various algorithms
+			double norm();
+			bool decompositionLU(Matrix& _L, Matrix& _U);
+			bool decompositionCholesky(Matrix& _L, Matrix& _Lt);
+			bool decompositionLDL(Matrix& _L, Matrix& _D, Matrix& _Lt);
+			bool decompositionQR_HR(Matrix& _Q, Matrix& _R);		// QR decomposition using Householder reflexions algorithm.
 			
 
-		private:
+		private:	// Private interface
 			int mCols, mRows;
 			type_* mPtr;
 
 		};
 
+		//-----------------------------------------------------------------------------
+		//--------------------------- Main interface ---------------------------------
 		//-----------------------------------------------------------------------------
 		template<typename type_> 
 		Matrix<type_>::Matrix(){
@@ -58,14 +68,14 @@ namespace BOViL{
 
 		//-----------------------------------------------------------------------------
 		template<typename type_> 
-		Matrix<type_>::Matrix(int _cols, int _rows):	mPtr(new type_[_cols*_rows]),
+		Matrix<type_>::Matrix(int _cols, int _rows):	mPtr(new type_[_cols*_rows]()),
 														mCols(_cols),
 														mRows(_rows)		
 		{}
 
 		//-----------------------------------------------------------------------------
 		template<typename type_> 
-		Matrix<type_>::Matrix(const type_* _matPtr, int _rows, int _cols):	mPtr(new type_[_cols*_rows]),
+		Matrix<type_>::Matrix(const type_* _matPtr, int _rows, int _cols):	mPtr(new type_[_cols*_rows]()),
 																			mCols(_cols),
 																			mRows(_rows)		
 		{
@@ -76,7 +86,7 @@ namespace BOViL{
 
 		//-----------------------------------------------------------------------------
 		template<typename type_> 
-		Matrix<type_>::Matrix(const Matrix<type_>& _mat):	mPtr(new type_[_mat.mCols*_mat.mRows]),
+		Matrix<type_>::Matrix(const Matrix<type_>& _mat):	mPtr(new type_[_mat.mCols*_mat.mRows]()),
 															mCols(_mat.mCols),
 															mRows(_mat.mRows) {
 			for(int i = 0; i < mCols*mRows ; i ++){
@@ -122,6 +132,8 @@ namespace BOViL{
 		}
 
 		//-----------------------------------------------------------------------------
+		//------------------------ Overloaded operators ------------------------------
+		//-----------------------------------------------------------------------------
 		template<typename type_>
 		std::string Matrix<type_>::operator<< (const Matrix<type_>& _mat) const{
 			
@@ -145,6 +157,12 @@ namespace BOViL{
 
 		//-----------------------------------------------------------------------------
 		template<typename type_>
+		type_* Matrix<type_>::operator[](int _index){
+			return (mPtr + _index);
+		}
+
+		//-----------------------------------------------------------------------------
+		template<typename type_>
 		void Matrix<type_>::operator= (const Matrix<type_>& _mat){
 			mRows = _mat.mRows;
 			mCols = _mat.mCols;
@@ -152,7 +170,7 @@ namespace BOViL{
 			if(mPtr)
 				delete mPtr;
 
-			mPtr = new type_[mRows*mCols];
+			mPtr = new type_[mRows*mCols]();
 
 			for(int i = 0 ; i < mRows*mCols ; i++){
 				mPtr[i] = _mat.mPtr[i];
@@ -169,7 +187,7 @@ namespace BOViL{
 			if(_mat.mCols != mCols || _mat.mRows != mRows)
 				assert(false);
 
-			type_* ptr = new type_[_mat.mRows*_mat.mCols];
+			type_* ptr = new type_[_mat.mRows*_mat.mCols]();
 
 			for(int i = 0; i < mRows ; i ++ ){
 				for(int j = 0 ; j < mCols ; j ++){
@@ -189,7 +207,7 @@ namespace BOViL{
 			if(_mat.mCols != mCols || _mat.mRows != mRows)
 				assert(false);
 
-			type_* ptr = new type_[_mat.mRows*_mat.mCols];
+			type_* ptr = new type_[_mat.mRows*_mat.mCols]();
 
 			for(int i = 0; i < mRows ; i ++ ){
 				for(int j = 0 ; j < mCols ; j ++){
@@ -209,7 +227,7 @@ namespace BOViL{
 			if(mCols !=_mat.mRows)
 				assert(false);
 
-			type_* ptr = new type_[mRows*_mat.mCols];
+			type_* ptr = new type_[mRows*_mat.mCols]();
 
 			for(int i = 0; i < mRows ; i ++ ){
 				for(int j = 0 ; j < mCols ; j ++){
@@ -225,12 +243,17 @@ namespace BOViL{
 
 			return mat;
 		}
+		//-----------------------------------------------------------------------------
+		template<typename type_>
+		Matrix<type_> Matrix<type_>::operator&(const Matrix<type_>& _mat) const{
+			// 666 TODO:
+		}
 
 		//-----------------------------------------------------------------------------
 		template<typename type_> 
 		Matrix<type_> Matrix<type_>::operator! () {
 			
-			type_* ptr = new type_[mRows*mCols];
+			type_* ptr = new type_[mRows*mCols]();
 
 			for(int i = 0; i < mRows ; i ++ ){
 				for(int j = 0 ; j < mCols ; j ++){
@@ -243,6 +266,98 @@ namespace BOViL{
 
 			return mat;
 		}
+
+			//-----------------------------------------------------------------------------
+		template<typename type_> 
+		type_ Matrix<type_>::operator~ () {
+			if(mCols == mRows){
+				Matrix<type_> L(mRows, mCols), U(mRows, mCols);
+
+				if(decompositionLU(L, U)){
+					type_ det = *U[0];
+					for(int i = 1 ; i < mRows ; i++){
+						det *= *U[i*mRows + i];
+					}
+					return det;
+				}
+			}
+			return NULL;
+		}
+
+		//-----------------------------------------------------------------------------
+		//-------------------------- Various algorithms -------------------------------
+		//------------------------------------------------------------------------------
+		template<typename type_>
+		double Matrix<type_>::norm(){		// 666 TODO: only true if vector, if not, is not max norm
+			int size = mRows > mCols ? mRows : mCols;
+
+			double norm = 0;
+
+			for(int i = 0 ; i < size ; i++){
+				norm += mPtr[i]*mPtr[i];
+			}
+
+			return sqrt(norm);
+		}
+
+		//------------------------------------------------------------------------------
+		template<typename type_>
+		bool Matrix<type_>::decompositionLU(Matrix& _L, Matrix& _U){
+			if(	_L.mCols != _U.mCols || _L.mCols != mCols ||
+				_L.mRows != _U.mRows || _L.mRows != mRows){
+				return false;	// Bad matrix sizes
+			}
+
+			_U = Matrix<type_>(mPtr, mRows, mCols);
+
+			for(int i = 0 ; i < mRows ; i++){
+				*_L[i*mCols + i] = 1.0;	// Set de diagonals of L. Eye matrix
+				for(int k = i + 1 ; k < mRows ; k++){
+					*_L[k*mCols + i] = *_U[k*mCols + i] / *_U[i*mCols + i];
+					for(int j = 0 ; j < mCols ; j ++){
+						*_U[k*mCols + j] = *_U[k*mCols + j] - *_U[i*mCols + j] * *_L[k*mCols + i];
+					}
+				}
+			}
+
+			return true;
+		}
+
+		//-----------------------------------------------------------------------------
+		template<typename type_>
+		bool Matrix<type_>::decompositionQR_HR(Matrix<type_>& _Q, Matrix<type_>& _R){
+				int t = m - 1 < n ? m - 1 : n;
+
+				Matrix<type_> Qs[t];
+
+		}
+		//-----------------------------------------------------------------------------
+
+		//-----------------------------------------------------------------------------
+
+		//-----------------------------------------------------------------------------
+		//-------------------------- Related functions -------------------------------
+		//-----------------------------------------------------------------------------
+		template<typename type_>
+		Matrix<type_> createEye(int _n){
+			Matrix<type_> mat(_n, _n);
+
+			for(int i = 0 ; i < _n ; i++){
+				*mat[i * _n + i] = 1;
+			}
+
+			return mat;
+		}
+
+		//-----------------------------------------------------------------------------
+		template<typename type_>
+		Matrix<type_> createGivenRotation(int _n, double _theta){
+			Marix<type_>  mat(_n, _n);
+
+			
+
+		}
+
 	}	// namespace math
 }	// namespace BOViL
 
