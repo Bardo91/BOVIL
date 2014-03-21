5a9fe9754cb94412f40cfba572261993fd348ca4
Bardo91 <19Bardo91@gmail.com>
2014-03-21 02:14:35 +0100

    MADE: --> Solving trouble with heritage and new

--- source/algorithms/state_estimators/ExtendedKalmanFilter.cpp
+++ source/algorithms/state_estimators/ExtendedKalmanFilter.cpp
@@ -18,7 +18,15 @@ namespace BOViL{
 		
 		//-----------------------------------------------------------------------------
 		void ExtendedKalmanFilter::setUpEKF(const math::Matrix<double> _Q, const math::Matrix<double> _R, const math::Matrix<double> _x0){
-			
+			mQ = _Q;
+			mR = _R;
+			mXak = _x0;
+			mXfk = _x0;
+			mK = math::createEye<double>(_Q.getHeight());;
+			mJf = math::createEye<double>(_Q.getHeight());;
+			mP = math::createEye<double>(_Q.getHeight());
+			mHZk = math::Matrix<double>(_Q.getHeight(), 1);
+			mJh = math::Matrix<double>(_R.getHeight(), _Q.getHeight());
 		}
 		
 		//-----------------------------------------------------------------------------
@@ -46,8 +54,13 @@ namespace BOViL{
 		void ExtendedKalmanFilter::filterStep(const math::Matrix<double>& _Zk){
 			updateHZk();
 			updateJh();
-			
-			mK = mP * !mJh * ((mJh * mP * !mJh + mR)^-1);
+
+			math::Matrix<double> auxT(mJh.transpose());
+			mK = mJh * mP * auxT;
+			mK = mK + mR;
+			mK = mK^-1;
+			mK = mP * auxT * mK;
+
 			
 			mXak = mXfk + mK * (_Zk - mHZk);
 			
