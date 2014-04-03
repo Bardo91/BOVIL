329dfca2ae07d474c7c59d77ecb14052bc677f3a
Bardo91 <19Bardo91@gmail.com>
2014-04-03 11:31:06 +0200

    MADE: --> Forgot to add ClientSocket and ServerSocket files in last commit
    TODO:
    --> Same as last commit

---
+++ source/core/comm/ClientSocket.h
@@ -0,0 +1,40 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+//	BOVIL: main
+//
+//		Author: Pablo Ramón Soria
+//		Date:	2014-03-29
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef _BOVIL_CORE_COMM_CLIENTSOCKET_H_
+#define _BOVIL_CORE_COMM_CLIENTSOCKET_H_
+
+#include "Socket.h"
+
+namespace BOViL{
+	namespace comm{
+		//-----------------------------------------------------------------------------
+		class ClientSocket: public Socket{
+		public:
+			ClientSocket(const std::string _ip, const std::string _port);
+
+			int sendData(std::string _data);
+			std::string receiveData();
+
+		protected:
+			int initializeSocket();
+			int connectSocket();
+
+		private:
+			addrinfo *mPtr;
+
+			std::string mServerIp, mServerPort;
+		};	//	class ClientSocket
+		//-----------------------------------------------------------------------------
+
+		//-----------------------------------------------------------------------------
+	}	//	namespace comm
+}	//	namespace BOViL
+
+#endif	// _BOVIL_CORE_COMM_CLIENTSOCKET_H_
---
+++ source/core/comm/ServerSocket.h
@@ -0,0 +1,43 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+//	BOVIL: main
+//
+//		Author: Pablo Ramón Soria
+//		Date:	2014-03-27
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+#ifndef _BOVIL_CORE_COMM_SERVERSOCKET_H_
+#define _BOVIL_CORE_COMM_SERVERSOCKET_H_
+
+#include "Socket.h"
+
+namespace BOViL{
+	namespace comm{
+		//-----------------------------------------------------------------------------
+		class ServerSocket: public Socket{
+		public:
+			ServerSocket(const std::string _port);
+
+			int sendData(std::string _data);
+			std::string receiveData();
+
+			int listenClient();
+			int acceptClient();
+
+		protected:
+			int initializeSocket();
+			int connectSocket();
+			
+		private:
+			SOCKET mClientSocket;
+
+			std::string mPort;
+		};	//	class ServerSocket
+		//-----------------------------------------------------------------------------
+
+		//-----------------------------------------------------------------------------
+	}	//	namespace comm
+}	//	namespace BOViL
+
+#endif	// _BOVIL_CORE_COMM_SERVERSOCKET_H_
