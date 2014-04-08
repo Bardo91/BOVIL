62d2bf31a328f86c6842b810cf16498fea8c27ae
Bardo91 <19Bardo91@gmail.com>
2014-04-08 09:12:18 +0200

    MADE: --> Configuring project for ekf and sockets

---
+++ Tests/test_ekf_sockets/server_side/src/main.cpp
@@ -0,0 +1,40 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+//	BOVIL: main
+//
+//		Author: Pablo Ramon Soria
+//		Date:	2014-03-12
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+#include "core/comm/ServerMultiThread.h"
+
+#include <vector>
+#include <map>
+#include <iostream>
+
+std::map<std::string, std::string> parseArgs(int _argc, char** _argv);
+
+int main(int _argc, char** _argv){
+	std::map<std::string, std::string> hashMap = parseArgs(_argc, _argv);
+
+	BOViL::comm::ServerMultiThread server(hashMap["PORT"]);
+
+
+	system("PAUSE");
+
+	return 0;
+
+}
+
+std::map<std::string, std::string> parseArgs(int _argc, char** _argv){
+	std::map<std::string, std::string> hashMap;
+	for (int i = 1; i < _argc; i++){
+		std::string arg(_argv[i]);
+		size_t next = arg.find_first_of('=');
+		std::string type = arg.substr(0, next);
+		arg = arg.substr(next + 1, arg.size());
+		hashMap[type] = arg;
+		std::cout << "Detected argument: " << type << " - With value: " << arg << std::endl;
+	}
+	return hashMap;
+}
