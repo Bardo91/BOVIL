142dc2f69709582db5940861fb12f5b9a99488c5
Bardo91 <19Bardo91@gmail.com>
2014-04-13 04:15:34 +0200

    MADE: --> Test for time functions
    TODO:
    --> Test on linux

---
+++ Tests/test_time/src/main.cpp
@@ -0,0 +1,48 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+//	BOVIL: main
+//
+//		Author: Pablo Ramon Soria
+//		Date:	2014-04-08
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+#include <core/time/time.h>
+
+#include <vector>
+#include <map>
+#include <iostream>
+#include <string>
+
+std::map<std::string, std::string> parseArgs(int _argc, char** _argv);
+
+int main(int _argc, char** _argv){
+	std::map<std::string, std::string> hashMap = parseArgs(_argc, _argv);
+
+	BOViL::STime::init();
+
+	BOViL::STime *time = BOViL::STime::get();
+
+	while (1){
+		std::cout << time->frameTime() << std::endl;
+	}
+
+	system("PAUSE");
+	
+	//delete client;	//	 666 TODO: add destructor.
+
+	return 0;
+
+}
+
+std::map<std::string, std::string> parseArgs(int _argc, char** _argv){
+	std::map<std::string, std::string> hashMap;
+	for (int i = 1; i < _argc; i++){
+		std::string arg(_argv[i]);
+		size_t next = arg.find_first_of('=');
+		std::string type = arg.substr(0, next);
+		arg = arg.substr(next + 1, arg.size());
+		hashMap[type] = arg;
+		std::cout << "Detected argument: " << type << " - With value: " << arg << std::endl;
+	}
+	return hashMap;
+}
