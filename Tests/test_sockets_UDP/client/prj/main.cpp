93005028b042a88c4485f35347fb276f5a1b8eef
Bardo91 <19Bardo91@gmail.com>
2014-05-29 23:15:20 +0200

    MADE: --> Started project for testing UDP sockets
    TODO:
    --> Error linking objects LNK4221

---
+++ Tests/test_sockets_UDP/client/prj/main.cpp
@@ -0,0 +1,37 @@
+//////////////////////////////////////////////////////////////////////////////////////////
+//																						//
+//		Test UDP socket																	//
+//			Date:	2014-05-29															//
+//			Author:	Pablo Ramon Soria													//
+//																						//
+//////////////////////////////////////////////////////////////////////////////////////////
+
+#include <core/comm/ClientSocket.h>
+#include <map>
+#include <string>
+
+using namespace BOViL::comm;
+using namespace std;
+
+std::map<std::string, std::string> parseArgs(int _argc, char** _argv);
+
+int main(int _argc, char** _argv){
+	map<string, string> hashMap = parseArgs(_argc, _argv);
+
+	ClientSocket<eSocketType::eUDP>* clientSocket = Socket::createClientSocket<eSocketType::eUDP>(hashMap["IP"], hashMap["PORT"]);
+
+	clientSocket;
+}
+
+std::map<std::string, std::string> parseArgs(int _argc, char** _argv){
+	std::map<std::string, std::string> hashMap;
+	for (int i = 1; i < _argc; i++){
+		std::string arg(_argv[i]);
+		size_t next = arg.find_first_of('=');
+		std::string type = arg.substr(0, next);
+		arg = arg.substr(next + 1, arg.size());
+		hashMap[type] = arg;
+		std::cout << "Detected argument: " << type << " - With value: " << arg << std::endl;
+	}
+	return hashMap;
+}
