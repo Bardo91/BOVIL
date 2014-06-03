f2a7b8e8aec9c0793caaad4623cec99a9c18ea2b
Bardo91 <19Bardo91@gmail.com>
2014-06-03 17:51:21 +0200

    MADE: --> Fixes for crosscompiling sockets

--- library/source/core/comm/ClientSocketUDP.cpp
+++ library/source/core/comm/ClientSocketUDP.cpp
@@ -32,51 +32,15 @@ namespace BOViL{
 			// will send datagrams to. For demonstration purposes, let's
 			// assume our receiver's IP address is _serverIp and waiting
 			// for datagrams on port _serverPort.
-			mAddr.sin_family = AF_INET;
-			mAddr.sin_port = htons((unsigned short)atoi(_serverPort.c_str()));
-			mAddr.sin_addr.s_addr = inet_addr(_serverIp.c_str());
-
+			// 666 TODO: bindear al puerto para no tener que hacer el send!
+			//mAddr.sin_family = AF_INET;
+			//mAddr.sin_port = htons((unsigned short)atoi(_serverPort.c_str()));
+			//#if defined(_WIN32)
+			//	mAddr.sin_addr.s_addr = inet_addr(_serverIp.c_str());
+			//#elif defined(__linux__)
+			//	mAddr.sin_addr.s_addr = htons(_serverIp.c_str());
+			//#endif
 			// 666 TODO: ver como meter el request de información del otro lado del socket etc...
-
-			//-------------------------------------------------------------------------------------
-			//memset(&mHints, 0, sizeof(mHints));
-			//mHints.ai_family = AF_INET;			// Connect to ip v4 or v6
-			//mHints.ai_socktype = SOCK_DGRAM;	// Connection tipe UDP IP
-			//mHints.ai_protocol = IPPROTO_UDP;
-			//
-			//// Geting address info
-			//int iResult = getaddrinfo(mServerIp.c_str(), mServerPort.c_str(), &mHints, &mResults);
-			//if (iResult != 0) {
-			//	closeSocket();
-			//
-			//	assert(false);
-			//}
-			//
-			//// Initializing socket
-			//// Attempt to connect to an address until one succeeds
-			//for (mPtr = mResults; mPtr != NULL; mPtr = mPtr->ai_next) {
-			//	// Create a SOCKET for connecting to server
-			//	mSocket = socket(mPtr->ai_family, mPtr->ai_socktype, mPtr->ai_protocol);
-			//	if (mSocket == INVALID_SOCKET) {
-			//		closeSocket();
-			//		assert(false);
-			//	}
-			//
-			//	if (bind(mSocket, mPtr->ai_addr, mPtr->ai_addrlen) == SOCKET_ERROR) {
-			//		freeaddrinfo(mPtr);
-			//		closeSocket();
-			//		continue;
-			//	}
-			//
-			//	break;
-			//}
-			//
-			//freeaddrinfo(mResults);
-			//
-			//if (mSocket == INVALID_SOCKET) {
-			//	closeSocket();
-			//	assert(false);
-			//}
 		}
 	}	//	namespace comm
 }	//	namespace BOViL
--- library/source/core/comm/ServerSocketUDP.cpp
+++ library/source/core/comm/ServerSocketUDP.cpp
@@ -40,37 +40,8 @@ namespace BOViL{
 
 			}
 
-
 			// 666 TODO: ver como meter el request de información del otro lado del socket etc...
 
-			//-------------------------------------------------------------------------------------
-			//memset(&mHints, 0, sizeof(mHints));
-			//mHints.ai_family = AF_INET;
-			//mHints.ai_socktype = SOCK_DGRAM;
-			//mHints.ai_protocol = IPPROTO_UDP;
-			//mHints.ai_flags = AI_PASSIVE;
-			//
-			//// Resolve the server address and port
-			//if (getaddrinfo(NULL, mServerPort.c_str(), &mHints, &mResult) != 0) {
-			//	closeSocket();
-			//	assert(false);
-			//}
-			//
-			//// Create a SOCKET for connecting to server
-			//mSocket = socket(mResult->ai_family, mResult->ai_socktype, mResult->ai_protocol);
-			//if (mSocket == INVALID_SOCKET) {
-			//	closeSocket();
-			//	freeaddrinfo(mResult);
-			//	assert(false);
-			//}
-			//
-			//if (bind(mSocket, mResult->ai_addr, mResult->ai_addrlen) == SOCKET_ERROR) {
-			//	freeaddrinfo(mResult);
-			//	closeSocket();
-			//	assert(false);
-			//}
-			//
-			//freeaddrinfo(mResult);
 		}
 	}	//	namespace comm
 }	//	namespace BOViL
