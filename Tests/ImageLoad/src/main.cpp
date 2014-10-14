c44d7c359b0037bd076065f4349fb68a059ffbf1
Bardo91 <19Bardo91@gmail.com>
2014-10-14 23:50:51 +0200

    CV vs own image load time

--- Tests/ImageLoad/src/main.cpp
+++ Tests/ImageLoad/src/main.cpp
@@ -6,15 +6,31 @@
 //			Date:	2014-10-14
 /////////////////////////////////////////////////////////////////////////////////////////
 //
-
+#include <core/time/time.h>
 #include <core/types/Image.h>
 #include <iostream>
 
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+
+
 using namespace BOViL;
+using namespace cv;
 
 int main(){	
 
+	STime::init();
+	STime *time = STime::get();
+
+	double t0 = time->getTime();
 	Image img("C:/Tulips.jpg");
+	double t1 = time->getTime();
+	Mat imageCV = imread("C:/Tulips.jpg");
+	double t2 = time->getTime();
+
+	std::cout << "My time: " << t1 - t0 << std::endl;
+	std::cout << "CV time: " << t2 - t1 << std::endl;
 
 	system("PAUSE");
 
