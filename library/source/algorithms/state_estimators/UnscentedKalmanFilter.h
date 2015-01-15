c46a3b4d3e8cd313c98bff43a85de3af5ce842ba
Bardo91 <19Bardo91@gmail.com>
2015-01-15 09:23:05 +0100

    Created UKF interface and private member's "interface"

---
+++ library/source/algorithms/state_estimators/UnscentedKalmanFilter.h
@@ -0,0 +1,41 @@
+/////////////////////////////////////////////////////////////////////////////////////////
+//	BOVIL: algorithms
+//
+//			Author:		Pablo Ramon Soria
+//			Date:		2015-01-15
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef _BOVIL_ALGORITHMS_STATE_ESTIMATORS_UNSCENTED_KALMAN_FILTER_H_
+#define _BOVIL_ALGORITHMS_STATE_ESTIMATORS_UNSCENTED_KALMAN_FILTER_H_
+
+#include "../../core/math/Matrix.h"
+
+namespace BOViL{
+	namespace algorithms{
+		class UnscentedKalmanFilter{
+		public:	// Public Interface
+			UnscentedKalmanFilter();		// 666 TODO: initialize matrixes
+			void setUpUKF(/* something */);
+			
+			void stepEKF(const math::Matrix<double>& _Zk, const double _incT);
+
+			math::Matrix<double> getStateVector() const;
+
+		private:	// Private methods
+			void sigmaPoints();
+			void forecastStep();
+			void dataStep();
+
+		private:	// Abstract private methods
+			virtual void updateJf(const double _incT) = 0;
+			virtual void updateHZk() = 0;
+			virtual void updateJh() = 0;
+
+		private:	// Private members
+
+		};
+	}	//	namespace comm
+}	//	namespace BOViL
+
+#endif	// _BOVIL_ALGORITHMS_STATE_ESTIMATORS_UNSCENTED_KALMAN_FILTER_H_
