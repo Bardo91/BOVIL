7861434d5b0dda4389a59213dd02a3d39a0c5007
Bardo91 <19Bardo91@gmail.com>
2015-01-17 17:14:22 +0100

    Replaced all examples of matrix test from BOViL matrix class to eigen class.

--- Tests/test_matrix/source/TestMatrix.cpp
+++ Tests/test_matrix/source/TestMatrix.cpp
@@ -8,116 +8,92 @@
 
 #include "TestMatrix.h"
 
+
+
+using namespace Eigen;
+using namespace std;
+
 void testMatrix(){
-	std::cout << "--------- TESTING MATRIX OPERATIONS --------------" << std::endl;
+	cout << "--------- TESTING MATRIX OPERATIONS --------------" << endl;
 	double matArray[9] = {	1.0, 2.0, 3.0, 
 							4.0, 5.0, 6.0, 
 							7.0, 8.0, 8.0	};
 
-	std::cout << "Matrix creation" << std::endl;
-	BOViL::math::Matrix<double> mat1(matArray, 3, 3);
-	mat1.showMatrix();
+	cout << "Matrix creation" << endl;
+	Matrix<double, 3, 3> mat;
+	cout << mat << endl;
 
-	std::cout << "------------" << std::endl;
-	std::cout << "Matrix asignation" << std::endl;
-	BOViL::math::Matrix<double> mat2;
-	mat2 = mat1;
-	mat2.showMatrix();
 
-	std::cout << "------------" << std::endl;
-	std::cout << "Matrix addition" << std::endl;
-	BOViL::math::Matrix<double> mat3 = mat1 + mat2;
-	mat3.showMatrix();
+	mat <<	1.0, 2.0, 3.0,
+			4.0, 5.0, 6.0,
+			7.0, 8.0, 8.0;
+	cout << mat << endl;
 
-	std::cout << "------------" << std::endl;
-	std::cout << "Matrix multiplication" << std::endl;
-	BOViL::math::Matrix<double> mat4 = mat1 * mat3;
-	mat4.showMatrix();
+	cout << "------------" << endl;
+	cout << "Matrix addition" << endl;
+	MatrixXd mat1 = mat + mat;
+	cout << mat1 << endl;
 
-	std::cout << "------------" << std::endl;
-	std::cout << "Matrix transpose" << std::endl;
-	BOViL::math::Matrix<double> mat5 = mat4.transpose();
-	mat5.showMatrix();
+	cout << "------------" << endl;
+	cout << "Matrix multiplication" << endl;
+	mat1 = mat*mat;
+	cout << mat1 << endl;
 
-	std::cout << "------------" << std::endl;
-	std::cout << "Matrix LU decomposition" << std::endl;
-	BOViL::math::Matrix<double> matL(3, 3), matU(3, 3);
-	if(mat1.decompositionLU(matL, matU)){
-		matL.showMatrix();
-		matU.showMatrix();
-	}
+	cout << "------------" << endl;
+	cout << "Matrix transpose" << endl;
+	mat1 = mat.transpose();
+	cout << mat1 << endl;
 
-	std::cout << "Determinant: " << mat1.determinant() << std::endl;
+	cout << "------------" << endl;
+	cout << "Matrix LU decomposition" << endl;
 
-	std::cout << "------------" << std::endl;
-	std::cout << "Norm" << std::endl;
-	BOViL::math::Matrix<double> mat6(matArray, 3, 1);	
-	mat6.showMatrix();
-	std::cout << "Norm: " << mat6.norm() << std::endl;
+	PartialPivLU<Matrix<double, 3, 3>> lu(mat);
+	
+	Matrix<double, 3, 3> l;
+	Matrix<double, 3, 3> u;
 
-	std::cout << "------------" << std::endl;
-	std::cout << "Eye matrix" << std::endl;
-	BOViL::math::Matrix<double> mat7 = BOViL::math::createEye<double>(3);	
-	mat7.showMatrix();
+	l = lu.matrixLU().triangularView<StrictlyLower>();
+	l += Matrix<double, 3, 3>::Identity();
+	u = lu.matrixLU().triangularView<Upper>();
+	
+	cout << l << endl;
+	cout << u << endl;
 
-	std::cout << "------------" << std::endl;
-	std::cout << "Scale Matrix" << std::endl;
-	BOViL::math::Matrix<double> mat8 = mat7*0.25;	
-	mat8.showMatrix();
-
-	double theta = atan(4.0/6.0);
-
-	std::cout << "------------" << std::endl;
-	std::cout << "Givens Rotation Matrix with theta: " << theta << " i = 1; j = 2" << std::endl;
-	BOViL::math::Matrix<double> mat9 = BOViL::math::createGivenRotation(3, 1, 2, theta);	
-	mat9.showMatrix();
-
-	std::cout << "------------" << std::endl;
-	std::cout << "Matrix QR decomposition with Givens Rotation" << std::endl;
-	BOViL::math::Matrix<double> matQ(3, 3), matR(3, 3);
-	if(mat1.decompositionQR_GR(matQ, matR)){
-		matQ.showMatrix();
-		matR.showMatrix();
-
-		BOViL::math::Matrix<double> matRes = matQ*matR;
-		matRes.showMatrix();
-	}
-	std::cout << "------------" << std::endl;
-	std::cout << "Test inverse of: " << std::endl;
-	double array3[16] = {	11122, 2222, 3, 4,
-							1222333, 12222, 3, 22111,
-							122232, 42222, 2222, 3,
-							1, 23, 33334, 2};
-
-	BOViL::math::Matrix<double> mat2inv(array3, 4, 4);
-	mat2inv.showMatrix();
-	std::cout << "is: " << std::endl;
-	BOViL::math::Matrix<double> matInv = mat2inv^-1;
-	matInv.showMatrix();
-
-	std::cout << "----- TESTING NON SQUARE MATRIX --------" << std::endl;
-
-	std::cout << "------------" << std::endl;
-	double array2[6] = {	1.0, 2.0, 3.0,
-							4.0, 5.0, 6.0	};
-
-	BOViL::math::Matrix<double> mat10(array2, 2, 3);
-	mat10.showMatrix();
-
-	std::cout << "------------" << std::endl;
-	std::cout << "Transpose NS matrix" << std::endl;
-	BOViL::math::Matrix<double> mat11 = mat10.transpose();
-	mat11.showMatrix();
-
-	std::cout << "------------" << std::endl;
-	std::cout << "Multiply NS matrixes" << std::endl;
-	BOViL::math::Matrix<double> mat12 = mat10*mat11;
-	mat12.showMatrix();
-
-	std::cout << "------------" << std::endl;
-	std::cout << "Multiply NS matrix" << std::endl;
-	BOViL::math::Matrix<double> mat13 = mat11*mat10;
-	mat13.showMatrix();
 
 	
+	cout << "Determinant: " << mat1.determinant() << endl;
+
+	cout << "------------" << endl;
+	cout << "Norm: " << mat.norm() <<endl;
+
+
+	cout << "------------" << endl;
+	cout << "Eye matrix" << endl;
+	cout << Matrix<double, 3, 3>::Identity() << endl;
+	
+	/* 
+	666 not supported initially
+	
+	cout << "------------" << endl;
+	cout << "Givens Rotation Matrix with theta: " << theta << " i = 1; j = 2" << endl;
+	BOViL::math::Matrix<double> mat9 = BOViL::math::createGivenRotation(3, 1, 2, theta);	
+	mat9.showMatrix();
+	*/
+
+	
+	cout << "------------" << endl;
+	cout << "Matrix QR decomposition" << endl;
+	HouseholderQR<Matrix<double,3,3>> qr(mat);
+
+	Matrix<double, 3, 3> q = qr.householderQ();
+	Matrix<double, 3, 3> r = qr.matrixQR().triangularView<Upper>();
+
+	cout << q << endl;
+	cout << r << endl;
+	
+	
+	cout << "------------" << endl;
+	cout << "Test inverse of: " << endl;
+	
+	cout << mat.inverse() << endl;
 }
