95812ccd9779bf44ebd3ff3bbd018810e013d4e7
Ubuntu12.04 <bardo91@ubuntu.(none)>
2015-03-29 12:19:57 +0200

    Erased dmc namespace in file and Json classes. File and Json classes were coded by Carmelo J for Domocracy's library. Erased dmc in order to clean library.

--- library/source/core/types/file/file.h
+++ library/source/core/types/file/file.h
@@ -9,38 +9,34 @@
 
 #include <string>
 
-namespace dmc {
+class File {
+public:
+	File(const std::string& _path); // File must exist
+	~File();
 
-	class File {
-	public:
-		File(const std::string& _path); // File must exist
-		~File();
+	static File* openExisting(const std::string& _path); // Returns nullptr if the file doesn't exist already
 
-		static File* openExisting(const std::string& _path); // Returns nullptr if the file doesn't exist already
+	void			readAll		();
+	void			setContent	(const void* _buffer, size_t, bool _hardCopy = true);
 
-		void			readAll		();
-		void			setContent	(const void* _buffer, size_t, bool _hardCopy = true);
+	const void *	buffer		() const;
+	const char *	bufferAsText() const;
+	size_t			sizeInBytes	() const;
 
-		const void *	buffer		() const;
-		const char *	bufferAsText() const;
-		size_t			sizeInBytes	() const;
+private:
+	size_t		mSize = 0;
+	const void*	mBuffer = nullptr;
+	bool		mMustWrite = false;
+	std::string	mPath;
+};
 
-	private:
-		size_t		mSize = 0;
-		const void*	mBuffer = nullptr;
-		bool		mMustWrite = false;
-		std::string	mPath;
-	};
+typedef File FileBase;
 
-	typedef File FileBase;
-
-	//------------------------------------------------------------------------------------------------------------------
-	// Inline implementation
-	//------------------------------------------------------------------------------------------------------------------
-	inline const void * File::buffer		() const { return mBuffer; }
-	inline const char * File::bufferAsText	() const { return reinterpret_cast<const char*>(mBuffer); }
-	inline size_t		File::sizeInBytes	() const { return mSize; }
-
-}
+//------------------------------------------------------------------------------------------------------------------
+// Inline implementation
+//------------------------------------------------------------------------------------------------------------------
+inline const void * File::buffer		() const { return mBuffer; }
+inline const char * File::bufferAsText	() const { return reinterpret_cast<const char*>(mBuffer); }
+inline size_t		File::sizeInBytes	() const { return mSize; }
 
 #endif // _DMCLIB_CORE_PLATFORM_FILE_FILE_H_
--- library/source/core/types/file/json.h
+++ library/source/core/types/file/json.h
@@ -10,81 +10,79 @@
 #include <unordered_map>
 #include <string>
 #include <vector>
+class Json {
+public:
+	// Internal types
+	typedef std::unordered_map<std::string, Json*>	Dictionary;
+	typedef std::vector<Json*>	List;
 
-namespace dmc {
-	class Json {
-	public:
-		// Internal types
-		typedef std::unordered_map<std::string, Json*>	Dictionary;
-		typedef std::vector<Json*>	List;
+	// Construction and asignment
+	Json() = default;
+	~Json();
+	explicit Json(const std::string& _code);
+	Json(const Json&);
+	Json& operator=(const Json&);
 
-		// Construction and asignment
-		Json() = default;
-		~Json();
-		explicit Json(const std::string& _code);
-		Json(const Json&);
-		Json& operator=(const Json&);
-
-		// Serialize
-		static Json	openFromFile	(const std::string& _fileName);
-		void		saveToFile		(const std::string& _fileName) const;
-		std::string serialize		() const;
+	// Serialize
+	static Json	openFromFile	(const std::string& _fileName);
+	void		saveToFile		(const std::string& _fileName) const;
+	std::string serialize		() const;
 
 
-		// Type deduction
-		bool isNill			() const { return mType == DataType::nill; }
-		bool isInt			() const { return mType == DataType::integer; }
-		bool isFloat		() const { return mType == DataType::real; }
-		bool isText			() const { return mType == DataType::text; }
-		bool isDictionary	() const { return mType == DataType::dictionary; }
-		bool isList			() const { return mType == DataType::list; }
-		bool isBool			() const { return mType == DataType::boolean; }
+	// Type deduction
+	bool isNill			() const { return mType == DataType::nill; }
+	bool isInt			() const { return mType == DataType::integer; }
+	bool isFloat		() const { return mType == DataType::real; }
+	bool isText			() const { return mType == DataType::text; }
+	bool isDictionary	() const { return mType == DataType::dictionary; }
+	bool isList			() const { return mType == DataType::list; }
+	bool isBool			() const { return mType == DataType::boolean; }
 
-		// Simple getters and setters
-		int					asInt		() const;
-		int&				asInt		();
-		void				setInt		(int);
-		float				asFloat		() const;
-		float&				asFloat		();
-		void				setFloat	(float);
-		const std::string&	asText		() const;
-		std::string&		asText		();
-		void				setText		(const std::string&);
-		bool				asBool		() const;
-		void				setBool		(bool);
+	// Simple getters and setters
+	int					asInt		() const;
+	int&				asInt		();
+	void				setInt		(int);
+	float				asFloat		() const;
+	float&				asFloat		();
+	void				setFloat	(float);
+	const std::string&	asText		() const;
+	std::string&		asText		();
+	void				setText		(const std::string&);
+	bool				asBool		() const;
+	void				setBool		(bool);
 
-		// Dictionary accessors
-		const Json&			operator[]	(const std::string& _key) const;
-		Json&				operator[]	(const std::string& _key);
-		bool				contains	(const std::string& _key) const;
-		const Dictionary&	asDictionary() const;
-		Dictionary&			asDictionary();
-		// List accessors
-		const Json&			operator[]	(unsigned _idx) const;
-		Json&				operator[]	(unsigned _idx);
-		void				push_back	(const Json& _obj);
-		const List&			asList		() const;
-		List&				asList		();
+	// Dictionary accessors
+	const Json&			operator[]	(const std::string& _key) const;
+	Json&				operator[]	(const std::string& _key);
+	bool				contains	(const std::string& _key) const;
+	const Dictionary&	asDictionary() const;
+	Dictionary&			asDictionary();
+	// List accessors
+	const Json&			operator[]	(unsigned _idx) const;
+	Json&				operator[]	(unsigned _idx);
+	void				push_back	(const Json& _obj);
+	const List&			asList		() const;
+	List&				asList		();
 
-	private:
-		unsigned initWithCode(const std::string& _code); // Returns number of parsed characters
+private:
+	unsigned initWithCode(const std::string& _code); // Returns number of parsed characters
 
-		enum class DataType {
-			nill,
-			integer,
-			real,
-			text,
-			dictionary,
-			list,
-			boolean
-		} mType = DataType::nill;
+	enum class DataType {
+		nill,
+		integer,
+		real,
+		text,
+		dictionary,
+		list,
+		boolean
+	} mType = DataType::nill;
 
-		std::string mString;
-		int			mInt;
-		float		mFloat;
-		Dictionary	mDictionary;
-		List		mList;
-	};
-}
+	std::string mString;
+	int			mInt;
+	float		mFloat;
+	Dictionary	mDictionary;
+	List		mList;
+};
+
 
 #endif // _DMCLIB_CORE_COMM_JSON_JSON_H_
