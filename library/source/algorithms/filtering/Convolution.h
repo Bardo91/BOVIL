5bc5593126837b60478be19a0d7e4fdb5a647918
Bardo91 <19Bardo91@gmail.com>
2015-04-09 02:08:55 +0200

    First version of image convolution, not tested

---
+++ library/source/algorithms/filtering/Convolution.h
@@ -0,0 +1,63 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//	
+//	BOVIL: 
+//		Author: Pablo Ramón Soria
+//		Date:	2015-APR-09
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+
+
+#ifndef BOVIL_ALGORITHMS_FILTERING_CONVOLUTION_H_
+#define BOVIL_ALGORITHMS_FILTERING_CONVOLUTION_H_
+
+#include <array>
+
+#include "Kernels.h"
+
+namespace BOViL{
+	namespace algorithms{
+		template<typename PixelType_, unsigned KernelSize_>
+		void ImageConvolution(	PixelType_*			_image,
+								unsigned					_width,
+								unsigned					_height,
+								unsigned					_channels,
+								Kernel<KernelSize_>			_kernel){
+			static_assert(double(KernelSize_) / 2 != 0.5);
+
+			PixelType_ *res = new PixelType_[_width*_height*_channels];
+
+			for (unsigned col = KernelSize_ / 2 + 1; col < _width - KernelSize_ / 2; col++) {
+				for (unsigned row = KernelSize_ / 2 + 1; row < _height - KernelSize_ / 2; row++){
+					for (unsigned channel = 0; channel < _channels; channel++){
+						// Extract subimage
+						std::array<PixelType_, KernelSize_ * KernelSize_> subImage;
+						for (unsigned i = -KernelSize_/2; i <= KernelSize_/2; i++){
+							for (unsigned j = KernelSize_/2; j <= KernelSize_/2; j++){
+								unsigned pixelIndex = row*_width*_channels + col*_channels + channel;
+								subImage[i*KernelSize_ + j] = _image[pixelIndex + i*_width*_channels + j*_channels]
+							}
+						}
+						//Convolute subimage with kernel
+						res[row*_width*_channels + col*_channels + channel] = convolute(subImage, _kernel);
+					}
+				}
+			}
+			PixelType_ *auxPointerHolder = _image;
+			_image = res;
+
+			delete[] auxPointerHolder;
+		}
+
+		template<typename Type_, unsigned Size_>
+		Type_ convolute(std::array<Type_, Size_ * Size_> _kernel1, std::array<Type_, Size_ * Size_> _kernel2){
+			Type_ res = 0;
+			for (unsigned i = 0; i < Size_*Size_; i++){
+				res += _kernel1[i] * _kernel2[i];
+			}
+			return res;
+		}
+	}	//	namespace algorithms
+}	//	namespace BOViL
+
+#endif	//	BOVIL_ALGORITHMS_FILTERING_CONVOLUTION_H_
---
+++ library/source/algorithms/filtering/Kernels.h
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//	
+//	BOVIL: 
+//		Author: Pablo Ramón Soria
+//		Date:	2015-APR-09
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+
+#ifndef BOVIL_ALGORITHMS_FILTERING_KERNELS_H_
+#define BOVIL_ALGORITHMS_FILTERING_KERNELS_H_
+
+#include <array>
+
+namespace BOViL{
+	namespace algorithms{
+		
+		template<unsigned Size_>
+		using Kernel = std::array<double, Size_ * Size_>;
+
+		typedef Kernel<3> Kernel3;
+
+		const Kernel3 SobelH = { {	2,	1,	2, 
+									0,	0,	0,
+									-2,	-1,	-2} };
+
+		const Kernel3 SobelV = { {	-2, 0, 2,
+									-1, 0, 1,
+									-2, 0, 2 } };
+	}	//	namespace algorithms
+}	//	namespace BOViL
+
+#endif	//	BOVIL_ALGORITHMS_FILTERING_KERNELS_H_
