cf2845bf4496536b89e0332c5c4eee3e41828d8d
Bardo91 <19Bardo91@gmail.com>
2015-07-03 15:37:05 +0200

    Moved tests files to howtos folder

--- src/code/algorithms/machine_learning/Regression.h
+++ src/code/algorithms/machine_learning/Regression.h
@@ -9,6 +9,7 @@
 #define _BOVIL_ALGORITHMS_MACHINELEARNING_REGRESSION_H_
 
 #include <array>
+#include <Eigen/Eigen>
 #include <functional>
 #include <vector>
 
@@ -16,20 +17,23 @@ namespace BOViL {
 	namespace algorithms{
 		/// Base tamplate of Regression algorithms (i.e. LinealRegression, PolinomialRegression, Logistic Regression, 
 		/// etc.).
-		template<unsigned InputSize_, std::vector<unsigned> Polinom_, std::function<double (std::vector<double>)> Hypothesis_>
+		template<unsigned InputSize_, Eigen::Matrix<double, InputSize_, InputSize_> Polinom_, std::function<double (std::vector<double>)> Hypothesis_>
 		class Regression{
 		public:
 			/// \brief Traing network with given dataset.
 			/// \param _x inputs of datasets.
 			/// \param _y desired results for given inputs.
-			bool train(std::vector<std::array<double, InputSize_>> _x, std::vector<double> _y);
+			template <unsigned TrainSize_>
+			bool train(Eigen::Matrix<double, TrainSize_, InputSize_> _x, Eigen::Matrix<double, TrainSize_, 1> _y);
 
 			/// \brief Prediction of Regression.
 			/// \param Input values.
-			double evaluate(std::array<double, InputSize_> _x);
+			double evaluate(Eigen::Matrix<double, InputSize_, 1> _x);
 
 		};
 	}	//	namespace algorithms
 }	//	namespace BOViL
+
+#include "Regression.inl"
 
 #endif _BOVIL_ALGORITHMS_MACHINELEARNING_REGRESSION_H_
