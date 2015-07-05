e42c51f96bc340e0f77e30f9df2c16f740649744
Bardo91 <19Bardo91@gmail.com>
2015-07-05 22:47:46 +0200

    Changed Regression interface

--- src/code/algorithms/machine_learning/Regression.h
+++ src/code/algorithms/machine_learning/Regression.h
@@ -19,31 +19,29 @@ namespace BOViL {
 		/// Base tamplate of Regression algorithms (i.e. LinealRegression, PolinomialRegression, Logistic Regression, 
 		/// etc.). Now cost function is always square function. Regression takes as template arguments:
 		/// \tparam InputSize_ number of variables + 1. (ex. a0 + a1*x1 + a2*x2 --> InputSize_ = 3)
-		template<unsigned Nvar_, unsigned Nmonomials_>
+		template<typename Hypothesis_>
 		class Regression{
 		public:	// Public interface
-			/// Redefinition for easy usage.
-			typedef Polynomial<Nvar_, Nmonomials_> Hypothesyis;
-
 			/// Build a regression with the given hypothesys.
-			Regression(const Hypothesyis &_hypothesis);
+			/// \param _hypothesis Polinomial equation that defines the hypothesis
+			Regression(const Hypothesis_ &_hypothesis);
 
 			/// \brief Traing network with given dataset.
 			/// \tparam TrainSize_ size of training set
 			/// \param _x inputs of datasets.
 			/// \param _y desired results for given inputs.
+			/// \param _alpha gradient coefficient
+			/// \param _lambda regularization parameter
+			/// \param _maxIter maximum number of iteration allowed
 			template <unsigned TrainSize_>
-			bool train(const Eigen::Matrix<double, TrainSize_, Nmonomials_> &_x, const Eigen::Matrix<double, TrainSize_, 1> &_y, double _alpha, unsigned _maxIter = 150);
+			bool train(const Eigen::Matrix<double, TrainSize_, Hypothesis_::Monomials> &_x, const Eigen::Matrix<double, TrainSize_, 1> &_y, double _alpha, double _lambda, unsigned _maxIter = 150);
 
 			/// \brief Prediction of Regression.
 			/// \param Input values.
-			double evaluate(const Hypothesyis::Input &_x);
-
-		private:
-			Eigen::Matrix<double, Nmonomials_, 1> gradient();
+			double evaluate(const Hypothesis_::Input &_x);
 
 		private:	// Private members
-			Hypothesyis	mHypothesys;
+			Hypothesis_	mHypothesys;
 
 		};
 	}	//	namespace algorithms
