cd8104a9d649348707dcdea7189f36381f255ac7
Bardo91 <19Bardo91@gmail.com>
2015-07-05 22:02:28 +0200

    Cleaned Regression

--- src/code/algorithms/machine_learning/Regression.h
+++ src/code/algorithms/machine_learning/Regression.h
@@ -1,4 +1,4 @@
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
 //		BOViL: Bardo Open Vision Libraries
 //
@@ -10,31 +10,40 @@
 
 #include <array>
 #include <Eigen/Eigen>
-#include <functional>
 #include <vector>
+
+#include "Polynomial.h"
 
 namespace BOViL {
 	namespace algorithms{
 		/// Base tamplate of Regression algorithms (i.e. LinealRegression, PolinomialRegression, Logistic Regression, 
-		/// etc.). Regression takes as template arguments
-		/// \tparam InputSize_ number of parameters
-		/// \tparam Polinom_ superior triangular matrix that defines the hypothesys.
-		template<unsigned InputSize_>
+		/// etc.). Now cost function is always square function. Regression takes as template arguments:
+		/// \tparam InputSize_ number of variables + 1. (ex. a0 + a1*x1 + a2*x2 --> InputSize_ = 3)
+		template<unsigned Nvar_, unsigned Nmonomials_>
 		class Regression{
-		public:
+		public:	// Public interface
+			/// Redefinition for easy usage.
+			typedef Polynomial<Nvar_, Nmonomials_> Hypothesyis;
+
 			/// Build a regression with the given hypothesys.
-			Regression(const Eigen::Matrix<double, InputSize_, InputSize_> &_hypothesis);
+			Regression(const Hypothesyis &_hypothesis);
 
 			/// \brief Traing network with given dataset.
 			/// \tparam TrainSize_ size of training set
 			/// \param _x inputs of datasets.
 			/// \param _y desired results for given inputs.
 			template <unsigned TrainSize_>
-			bool train(const Eigen::Matrix<double, TrainSize_, InputSize_> &_x, const Eigen::Matrix<double, TrainSize_, 1> &_y);
+			bool train(const Eigen::Matrix<double, TrainSize_, Nmonomials_> &_x, const Eigen::Matrix<double, TrainSize_, 1> &_y, double _alpha, unsigned _maxIter = 150);
 
 			/// \brief Prediction of Regression.
 			/// \param Input values.
-			double evaluate(const Eigen::Matrix<double, InputSize_, 1> &_x);
+			double evaluate(const Hypothesyis::Input &_x);
+
+		private:
+			Eigen::Matrix<double, Nmonomials_, 1> gradient();
+
+		private:	// Private members
+			Hypothesyis	mHypothesys;
 
 		};
 	}	//	namespace algorithms
