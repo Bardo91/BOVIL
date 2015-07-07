4c4d6a36c61709efc3fc8b14666da31a7674313e
Bardo91 <19Bardo91@gmail.com>
2015-07-07 21:18:54 +0200

    Tested logisticRegression for multiple variables.

--- tests/algorithms/machineLearning/regression/test.cpp
+++ tests/algorithms/machineLearning/regression/test.cpp
@@ -17,6 +17,7 @@ void linearRegression();
 void polinomialRegression();
 void polinomialRegressionMulti();
 void logisticRegression();
+void logisticRegressionMulti();
 
 int main(int _argc, char** _argv) {
 
@@ -27,6 +28,8 @@ int main(int _argc, char** _argv) {
 	polinomialRegressionMulti();
 
 	logisticRegression();
+
+	logisticRegressionMulti();
 }
 
 //---------------------------------------------------------------------------------------------------------------------
@@ -113,8 +116,8 @@ void polinomialRegressionMulti() {
 
 //---------------------------------------------------------------------------------------------------------------------
 void logisticRegression() {
-	// ex. y = a + b*x1 + c*x1^2;
-	Regression<1,4>::Hypothesis hypothesis = [] (const Polynomial<1,3>::Input &_x) {
+	// ex. y = a + b*x1 + c*x1^2 + d*x1^3;
+	Regression<1,4>::Hypothesis hypothesis = [] (const Polynomial<1,4>::Input &_x) {
 		Polynomial<1,4>::Monomials mon;
 		mon << 1, _x(0), pow(_x(0),2), pow(_x(0),3);
 		return mon;
@@ -142,4 +145,57 @@ void logisticRegression() {
 	Matrix<double,1,1> z;
 	z << 1.8;
 	assert(regression.evaluate(z) < 0.5);
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+void logisticRegressionMulti() {
+	// ex. y = a + b*x1 + c*x1*x2 + d*x2 + e*x2^2
+	Regression<2,6>::Hypothesis hypothesis = [] (const Polynomial<2,6>::Input &_x) {
+		Polynomial<2,6>::Monomials mon;
+		mon << 1, _x(0), _x(0)*_x(0), _x(0)*_x(1), _x(1), pow(_x(1),2);
+		return mon;
+	};
+
+	auto sigmoid = [](double _x) {
+		return 1/(1 + exp(-_x));
+	};
+
+	Regression<2,6> regression(hypothesis, sigmoid);
+
+	Matrix<double, 25, 2> xTs;
+	xTs <<	0, 0,	// y = 0.
+			0, 0.5,	// y = 0.
+			0, 1,	// y = 0.
+			0, 1.5,	// y = 0.
+			0, 2,	// y = 0.
+			2, 0,	// y = 0.
+			2, 0.5,	// y = 0.
+			2, 1,	// y = 0.
+			2, 1.5,	// y = 0.
+			2, 2,	// y = 0.
+			0.5, 0,	// y = 0.
+			1, 0,	// y = 0.
+			1.5, 0,	// y = 0.
+			0.5, 2,	// y = 0.
+			1, 2,	// y = 0.
+			1.5, 2,	// y = 0.
+			0.75, 0.75,	// y = 1.
+			0.75, 1,	// y = 1.
+			0.75, 1.25,	// y = 1.
+			1, 0.75,	// y = 1.
+			1, 1,		// y = 1.
+			1, 1.25,	// y = 1.
+			1.25, 0.75,	// y = 1.
+			1.25, 1,	// y = 1.
+			1.25, 1.25;	// y = 1.
+
+	Matrix<double, 25, 1> yTs;
+	yTs << 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1;
+
+	regression.train<25>(xTs, yTs, 0.2, 0.0, 1000);
+
+	Matrix<double, 1, 2> x({1.2, 1.3});
+	assert(regression.evaluate(x) > 0.5);
+	Matrix<double, 1, 2> y({0.3, 1.9});
+	assert(regression.evaluate(y) < 0.5);
 }
