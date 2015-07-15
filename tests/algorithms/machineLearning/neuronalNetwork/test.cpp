2cce8c41c4b8dffc31f62a6f88f939330f74ac64
Bardo91 <19Bardo91@gmail.com>
2015-07-15 20:52:31 +0200

    Tested with different network structure.

--- tests/algorithms/machineLearning/neuronalNetwork/test.cpp
+++ tests/algorithms/machineLearning/neuronalNetwork/test.cpp
@@ -17,25 +17,25 @@ using namespace BOViL::algorithms;
 int main(int _argc, char** _argv) {
 
 	// Simple nn with 3 input units 1 output unit and 1 hidden layer with 4 units.
-	NeuronalNetwork<3,1,4,1> nn;
+	NeuronalNetwork<4,1,4,1> nn;
 
-	Matrix<double, 6, 3> xSet;
-	xSet << 1, 0, 0,
-			1, 0.5, 0,
-			1, 1, 0,
-			0, 0, 0,
-			0, 0, 0.5,
-			0, 0, 1;
-	Matrix<double, 6, 1> ySet;
-	ySet << 1, 1, 1, 0, 0, 0;
-	nn.train<6>(xSet, ySet, 1, 0.1);
+	Matrix<double, 7, 4> xSet;
+	xSet << 1, 0, 0,0,
+			1, 0.5, 0,0,
+			1, 1, 0,0,
+			0, 0, 0,0,
+			0, 0, 0.5,0,
+			0, 0, 1,0,
+			0, 0.5, 1,0;
+	Matrix<double, 7, 1> ySet;
+	ySet << 1, 1, 1, 0, 0, 0, 0;
+	nn.train<7>(xSet, ySet, 1, 0.1);
 
-	Matrix<double, 3, 1> x1({0.8, 0.5, 0});
-	Matrix<double, 1, 1> y1 = nn.evaluate(x1); // ~ 0.749078
+	Matrix<double, 4, 1> x1({0.8, 0.5, 0, 0});
+	Matrix<double, 1, 1> y1 = nn.evaluate(x1);
 	assert(y1(0,0) > 0.5);
-	std::cout << y1 << std::endl<< std::endl;
 
-	Matrix<double, 3, 1> x2({0, 0, 0.8});
-	Matrix<double, 1, 1> y2 = nn.evaluate(x2);	// ~ 0.116687
+	Matrix<double, 4, 1> x2({0, 0, 0.8, 0});
+	Matrix<double, 1, 1> y2 = nn.evaluate(x2);
 	assert(y2(0,0) < 0.5);
 }
