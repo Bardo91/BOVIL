f73d615d6116159be034ddf01d7b1474fe9427d4
Bardo91 <19Bardo91@gmail.com>
2015-07-17 11:13:20 +0200

    Added one more dataset to train NN (iris Dataset).

--- tests/algorithms/machineLearning/neuronalNetwork/test.cpp
+++ tests/algorithms/machineLearning/neuronalNetwork/test.cpp
@@ -17,12 +17,15 @@ using namespace BOViL::algorithms;
 
 void simpleNN();
 void wineNN();
+void irisNN();
 
 int main(int _argc, char** _argv) {
 
 	simpleNN();
 
 	wineNN();
+
+	irisNN();
 }
 
 void simpleNN() {
@@ -50,14 +53,39 @@ void simpleNN() {
 	assert(y2(0,0) < 0.5);
 }
 
-void loadDataset(Matrix<double, 178,13> &_xSet, Matrix<double, 178,3> &_ySet);
+template<unsigned TrainSize_, unsigned InputSize_, unsigned OutputSize_>
+void loadDataset(Matrix<double, TrainSize_,InputSize_> &_xSet, Matrix<double, TrainSize_,OutputSize_> &_ySet, std::string _file) {
+	std::ifstream dataFile(_file);
+	assert(dataFile.is_open());
+	std::string line;
+	unsigned i = 0;
+	_ySet = Matrix<double, TrainSize_,OutputSize_>::Zero();
+	while(std::getline(dataFile, line)) {
+		// Get result
+		int index = line.find(",");
+		_ySet(i,atoi(line.substr(0,index).c_str())-1) = 1;
+		line = line.substr(index+1);
+
+		// Get inputs
+		int j = 0;
+		do {
+			int index = line.find(",");
+			_xSet(i,j) = atof(line.substr(0,index).c_str());
+			line = line.substr(index+1);
+			j++;
+		}while(line.find(",") != std::string::npos);
+		_xSet(i,j) = atof(line.c_str());
+		i++;
+	}
+	dataFile.close();
+}
 
 void wineNN() {
 	NeuronalNetwork<13,1,20,3> nn;
 
 	Matrix<double, 178,13> xSet;
 	Matrix<double, 178,3> ySet;
-	loadDataset(xSet, ySet);
+	loadDataset<178,13,3>(xSet, ySet, "wineDataset.txt");
 
 	nn.train<178>(xSet, ySet, 1.0, 0.1);
 
@@ -78,31 +106,34 @@ void wineNN() {
 	Matrix<double, 1, 3> y3 = nn.evaluate(x3);	// y ~ [0 0 1] ;
 	assert(y3(2) > 0.5);
 	assert(y3(0) + y3(1) < 0.5);
-	
+
 }
 
-void loadDataset(Matrix<double, 178,13> &_xSet, Matrix<double, 178,3> &_ySet) {
-	std::ifstream dataFile("wineDataset.txt");
-	assert(dataFile.is_open());
-	std::string line;
-	unsigned i = 0;
-	_ySet = Matrix<double, 178,3>::Zero();
-	while(std::getline(dataFile, line)) {
-		// Get result
-		int index = line.find(",");
-		_ySet(i,atoi(line.substr(0,index).c_str())-1) = 1;
-		line = line.substr(index+1);
+void irisNN() {
+	NeuronalNetwork<4,1,20,3> nn;
 
-		// Get inputs
-		int j = 0;
-		do {
-			int index = line.find(",");
-			_xSet(i,j) = atof(line.substr(0,index).c_str());
-			line = line.substr(index+1);
-			j++;
-		}while(line.find(",") != std::string::npos);
-		_xSet(i,j) = atof(line.c_str());
-		i++;
-	}
-	dataFile.close();
+	Matrix<double, 150,4> xSet;
+	Matrix<double, 150,3> ySet;
+	loadDataset<150,4,3>(xSet, ySet,"irisDataset.txt");
+
+	nn.train<150>(xSet, ySet, 1.0, 0.1);
+
+	Matrix<double, 4, 1> x1;
+	x1 << 4.7,3.2,1.3,0.2;
+	Matrix<double, 1, 3> y1 = nn.evaluate(x1);	// y ~ [1 0 0] ;
+	assert(y1(0) > 0.5);
+	assert(y1(1) + y1(2) < 0.5);
+
+	Matrix<double, 4, 1> x2;
+	x2 << 4.9,2.4,3.3,1.0;
+	Matrix<double, 1, 3> y2 = nn.evaluate(x2);	// y ~ [0 1 0] ;
+	assert(y2(1) > 0.5);
+	assert(y2(0) + y2(2) < 0.5);
+
+	Matrix<double, 4, 1> x3;
+	x3 << 7.7,3.0,6.1,2.3;
+	Matrix<double, 1, 3> y3 = nn.evaluate(x3);	// y ~ [0 0 1] ;
+	assert(y3(2) > 0.5);
+	assert(y3(0) + y3(1) < 0.5);
+
 }
