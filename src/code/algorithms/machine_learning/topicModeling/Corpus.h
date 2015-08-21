e3412729c7465385a89b3dee9d46d069250c663a
Bardo91 <19Bardo91@gmail.com>
2015-08-21 23:23:26 +0200

    Documented Corpus class

--- src/code/algorithms/machine_learning/topicModeling/Corpus.h
+++ src/code/algorithms/machine_learning/topicModeling/Corpus.h
@@ -16,8 +16,12 @@ namespace BOViL {
 	namespace algorithms {
 		class Corpus {
 		public:
+			/// Add a document to the corpus
+			/// \param _document: document to be added to the corpus
 			void addDocument(Document _document) { mDocuments.push_back(_document); };
 
+			/// Get count of total words in document.
+			/// \return Sum of all words in all documents
 			unsigned totalWords() {
 				unsigned numWords = 0;
 				for (Document doc : mDocuments) {
@@ -25,9 +29,17 @@ namespace BOViL {
 				}
 				return numWords;
 			};
+
+			/// Get number of documents in the corpus
+			/// \return Number of documents in the corpus
 			unsigned numDocs() { return mDocuments.size(); };
 
+			/// Access to a document
+			/// \param index to the desired document.
 			Document document(unsigned _index) { return mDocuments[_index]; };
+
+			/// Shuffle word list.
+			void shuffle(){ std::random_shuffle(mDocuments.begin(), mDocuments.end()); };
 
 		private:
 			std::vector<Document> mDocuments;
