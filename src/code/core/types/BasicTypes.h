c1d828715c32ec1c4d39095f56587fc2e05ea2af
Bardo91 <19Bardo91@gmail.com>
2015-09-24 09:40:40 +0200

    Renamed method of Image object to smarter style.

--- src/code/core/types/BasicTypes.h
+++ src/code/core/types/BasicTypes.h
@@ -51,54 +51,46 @@ namespace BOViL{
 	typedef Vec3<unsigned char> Vec3uc;
 
 	//-----------------------------------------------------------------------------
-	/** Basic class that holds information of generic image object
-	*/
+	/// Basic class that holds information of generic image object
 	class ImageObject{		// Summarized object.
 	public:
-		/** \brief
-		*/
+		/// \brief
 		ImageObject(Vec2ui _upperLeft, Vec2ui _downRight, int _size, int _color){
-			centroid = Vec2ui((_upperLeft.x + _downRight.x)/2, (_upperLeft.y + _downRight.y)/2);
-			width = _downRight.x - _upperLeft.x;
-			height = _downRight.y - _upperLeft.y;
-			size = _size;
-			color = _color;
+			mCentroid = Vec2ui((_upperLeft.x + _downRight.x)/2, (_upperLeft.y + _downRight.y)/2);
+			mWidth = _downRight.x - _upperLeft.x;
+			mHeight = _downRight.y - _upperLeft.y;
+			mSize = _size;
+			mColor = _color;
 		};
 
-		/** \brief
-		*/
+		/// \brief
 		ImageObject(Vec2ui _centroid, unsigned _width, unsigned _height, int _size, int _color){
-			centroid = _centroid;
-			width = _width;
-			height = _height;
-			size = _size;
-			color = _color;
+			mCentroid = _centroid;
+			mWidth = _width;
+			mHeight = _height;
+			mSize = _size;
+			mColor = _color;
 		};
 			
-		/** \brief get centroid of object in the image. 666 rename to centroid();
-		*/
-		Vec2ui getCentroid() const {return centroid;};
+		/// \brief get centroid of object in the image. 666 rename to centroid();
+		Vec2ui centroid() const {return mCentroid;};
 		
-		/** \brief get width of object. 666 rename to width().
-		*/
-		int getWidth() const {return width;};
+		/// \brief get width of object. 666 rename to width().
+		int width() const {return mWidth;};
 		
-		/** \brief get height of object. 666 rename to height().
-		*/
-		int getHeight() const {return height;};
+		/// \brief get height of object. 666 rename to height().
+		int height() const {return mHeight;};
 		
-		/** \brief get color of object. 666 rename to color(). 666 Only used in CCS, need review.
-		*/
-		int getColor() const {return color;};
+		/// \brief get color of object. 666 rename to color(). 666 Only used in CCS, need review.
+		int color() const {return mColor;};
 		
-		/** \brief get number of pixels of the object. 666 rename to size().
-		*/
-		int getSize() const {return size;};
+		/// \brief get number of pixels of the object. 666 rename to size().
+		int size() const {return mSize;};
 	private:
-		Vec2ui centroid;
-		int width, height;
-		int color;
-		int size;
+		Vec2ui mCentroid;
+		int mWidth, mHeight;
+		int mColor;
+		int mSize;
 	};
 
 }	// namespace BOViL
