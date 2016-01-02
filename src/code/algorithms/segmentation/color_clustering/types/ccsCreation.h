8b8ee71679fb44985213fca8cf214b4c7a4e7edb
Bardo91 <19Bardo91@gmail.com>
2016-01-02 16:56:55 +0100

    New method to crate CCS that join in one category sparse clusters

--- src/code/algorithms/segmentation/color_clustering/types/ccsCreation.h
+++ src/code/algorithms/segmentation/color_clustering/types/ccsCreation.h
@@ -64,6 +64,66 @@ namespace BOViL{
 		return new ColorClusterSpace(_subdivision, AClass, BClass, CClass, colors);
 	}
 
+	//--------------------------------------------------------------------------------------------------------------------
+	ColorClusterSpace * createSingleSparseCluster(	const std::vector<std::pair<unsigned char, unsigned char>> & _c1Limits,
+													const std::vector<std::pair<unsigned char, unsigned char>> & _c2Limits,
+													const std::vector<std::pair<unsigned char, unsigned char>> & _c3Limits,
+													const unsigned char _c1Max,
+													const unsigned char _c2Max,
+													const unsigned char _c3Max,
+													const unsigned char _subdivision) {
+
+		if (_c1Limits.size() == 0 || _c2Limits.size() == 0 || _c3Limits.size() == 0)
+			return nullptr;
+		
+		// Channels thresholds.
+		unsigned char *AClass = new unsigned char[_subdivision];
+		unsigned char *BClass = new unsigned char[_subdivision];
+		unsigned char *CClass = new unsigned char[_subdivision];
+
+		unsigned char incC1 = _c1Max / _subdivision;
+		unsigned char incC2 = _c2Max / _subdivision;
+		unsigned char incC3 = _c3Max / _subdivision;
+
+		for (unsigned i = 0; i < _subdivision; i++) {
+			for (unsigned j = 0; j < _c1Limits.size(); j++) {
+				if ((i*incC1 >= _c1Limits[j].first) && (i*incC1 <= _c1Limits[j].second)) {
+					AClass[i] = 1;
+					break;
+				}
+				else {
+					AClass[i] = 0;
+				}
+			}
+			for (unsigned j = 0; j < _c2Limits.size(); j++) {
+				if ((i*incC2 >= _c2Limits[j].first) && (i*incC2 <= _c2Limits[j].second)) {
+					BClass[i] = 1;
+					break;
+				}
+				else {
+					BClass[i] = 0;
+				}
+			}
+			for (unsigned j = 0; j < _c3Limits.size(); j++) {
+				if ((i*incC3 >= _c3Limits[j].first) && (i*incC3 <= _c3Limits[j].second)) {
+					CClass[i] = 1;
+					break;
+				}
+				else {
+					CClass[i] = 0;
+				}
+			}
+		}
+
+		// Color dictionary.
+		c3u colors[8];
+		colors[0] = c3u((_c1Limits[0].first + _c1Limits[0].second) / 2,
+						(_c2Limits[0].first + _c2Limits[0].second) / 2,
+						(_c3Limits[0].first + _c3Limits[0].second) / 2);
+
+		return new ColorClusterSpace(_subdivision, AClass, BClass, CClass, colors);
+	}
+
 }
 
 #endif	//	BOVIL_ALGORITHMS_SEGMENTATION_COLOR_CLUSTERING_TYPES_COLOR_CLUSTERED_SPACE_CREATOR_H_
